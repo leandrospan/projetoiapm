fcdcdaee2e9c28e1b04eebcc5c1a537b4806d495
leandrospan <leandrospan@gmai.com>
2023-03-04 11:13:02 -0300

    Ajustado a liberação do acesso ao http/json

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+import { stringify } from 'querystring';
+
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +12,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  nome: string = '';
+  senha: string = '';
+  usuarios: any;
+  usuario: string = '';
+
+  constructor(private http: HttpClient, private alertController: AlertController) {}
+
+  async mensagem(m: string) {
+    const alert = await this.alertController.create({
+      header: 'Mensagem',
+      subHeader: 'Retorno',
+      message: m,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+
+  async verificar() {
+    await this.http.get<any>('http://192.168.1.112/projetoiap/apijson.php').subscribe(data => {
+      this.usuarios = data;
+      //this.mensagem(JSON.stringify(this.usuarios));
+      this.usuario = JSON.stringify(this.usuarios);
+      //this.mensagem((this.usuario.indexOf(this.nome).toString()));
+      // O indexOf faz a procura da string dentro de outra, se ele não encontrar ele retorna -1
+      if ((this.usuario.indexOf(this.nome) !== -1) && (this.usuario.indexOf(this.senha) !== -1)){
+        this.mensagem('Acesso Permitido');
+      } else {
+        this.mensagem('Acesso Negado');
+      }
+    });
+  }
 
 }
